fc199d6e7b1e46a4e2b177252603ba9da808fd5f
dan.pupius <dan.pupius@4c66444a-c113-11de-8ec6-755e00613368>
2009-10-25 06:04:51 +0000

    Copy paste snafu

--- trunk/fittr-flickr/features/light-box.js
+++ trunk/fittr-flickr/features/light-box.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */**
+ *
  * Light-Box
  * Allows you to quickly view the large version of a photo without having to switch pages.
  */
